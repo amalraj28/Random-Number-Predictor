b0f6b32daf9b1dedc28843f432262f94f6b0aaaa
Amal Raj <amalraj2701@gmail.com>
2022-12-27 00:25:39 +0530

    Added gifs

--- server.py
+++ server.py
@@ -16,11 +16,14 @@ def check_num(num: int) -> str:
     num = int(num)
 
     if num == random_number:
-        return '<h1>You win!!</h1>'
+        return '<h1>You win!!</h1>' \
+               '<iframe src="https://giphy.com/embed/jOytPtkbqgDmjQBpuR" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
     elif num > random_number:
-        return '<h1>Too high</h1>'
+        return '<h1>Too high</h1>' \
+               '<iframe src="https://giphy.com/embed/MwrQvTZA9Puuc" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
     else:
-        return '<h1>Too low</h1>'
+        return '<h1>Too low</h1>' \
+               '<iframe src="https://giphy.com/embed/3oEdv7Puv6s0eKvExW" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
 
 
 server.run(debug=True)
